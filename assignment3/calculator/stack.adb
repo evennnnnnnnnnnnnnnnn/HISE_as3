1108af64d79cd1b96cdca3d15ba6b5d56e1c592c
WeihaoLi-Tom <weihao4@student.unimelb.edu.au>
2025-05-24 23:53:45 +1000

    stack

--- assignment3/calculator/stack.adb
+++ assignment3/calculator/stack.adb
@@ -1,6 +1,7 @@
-with Ada.Text_IO;             use Ada.Text_IO;
+pragma SPARK_Mode (On);
 
 package body Stack is
+   
    procedure Initialize(S: out Stack_type) is
    begin
       S.Top := 0;
@@ -9,49 +10,35 @@ package body Stack is
    
    procedure Push(S: in out Stack_type; V: Integer) is
    begin
-      if Depth(S) >= Max_Stack then
+      if S.Top >= Max_Stack then
          raise Constraint_Error with "Stack overflow";
       end if;
+      
       S.Top := S.Top + 1;
       S.Data(S.Top) := V;
    end Push;
    
    procedure Push2(S: in out Stack_type; V1, V2: Integer) is
    begin
-      if Depth(S) > Max_Stack - 2 then
+      if S.Top > Max_Stack - 2 then
          raise Constraint_Error with "Stack overflow";
       end if;
-      Push(S, V1);
-      Push(S, V2);
+      
+      -- Direct implementation to avoid nested precondition issues
+      S.Top := S.Top + 1;
+      S.Data(S.Top) := V1;
+      S.Top := S.Top + 1;
+      S.Data(S.Top) := V2;
    end Push2;
    
    procedure Pop(S: in out Stack_type) is
    begin
-      if Depth(S) = 0 then
+      if S.Top = 0 then
          raise Constraint_Error with "Stack underflow";
       end if;
+      
       S.Top := S.Top - 1;
    end Pop;
    
-   function Top_Value(S: Stack_type) return Integer is
-   begin
-      if Depth(S) = 0 then
-         raise Constraint_Error with "Stack is empty";
-      end if;
-      return S.Data(S.Top);
-   end Top_Value;
    
-   function Second_Value(S: Stack_type) return Integer is
-   begin
-      if Depth(S) <= 1 then
-         raise Constraint_Error with "Stack has less than 2 elements";
-      end if;
-      return S.Data(S.Top - 1);
-   end Second_Value;
-   
-   function Depth(S: Stack_type) return Natural is
-   begin
-      return S.Top;
-   end Depth;
-
 end Stack;
